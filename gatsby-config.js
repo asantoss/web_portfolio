029caeb18c6899056c0b0722d0472d27b041a569
Alexander Santos <alexsantosantana@live.com>
2020-06-05 00:57:32 -0400

    adding more about me

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
 		// eslint-disable-next-line no-irregular-whitespace
 		about: `I'​m​ a full stack developer living in Atlanta with my wife and two beautiful doggos. I loves all things tech, my hobbies include building extravagant computers with crazy RGB and creating beautiful web applications that can fix everyday problems.I​’m​ experienced in
     multiple web technologies including HTML/CSS, JavaScript, ReactJS,
-    Node.JS GraphQL & PostgresQL. I also enjoy biking and long park days with my dogs. I'm a Certified Full Stack Developer and I’m seeking employment in Atlanta.`,
+    Node.JS GraphQL & PostgresQL. I also enjoy gaming and long park days with my dogs. I'm a Certified Full Stack Developer and I’m seeking employment in Atlanta.`,
 		projects: [
 			{
 				title: 'Client Manager',
