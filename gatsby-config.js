4b2a28c5454538ff0a459a114833ee8cfb485a6d
Alexander Santos <alexsantosantana@live.com>
2020-06-18 19:56:08 -0400

    Google plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,7 +49,7 @@ module.exports = {
 			},
 		},
 		{
-			resolve: `gatsby-pluging-google-analytics`,
+			resolve: `gatsby-plugin-google-analytics`,
 			options: {
 				trackingId: `UA-145109659-2`,
 			},
